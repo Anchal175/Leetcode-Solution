2d524f9730f08ca4f937aeba9d0efcb850713df6
anchal singh <anchal1222005@gmail.com>
2025-10-24 09:57:44 +0530

    Time: 161 ms (21.54%) | Memory: 44.5 MB (44.62%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -1,26 +1,27 @@
-// /
 class Solution {
     public int nextBeautifulNumber(int n) {
-        int num = n + 1;
-        while (true) {
-            if (isBalanced(num)) return num;
-            num++; // <-- yeh line zaruri hai
-        }
+     int num=n+1;
+     while(true){
+        if(balanced(num))
+        return num;
+        num++;
+     }
     }
+   
+    private boolean balanced(int num){
+     String s=String.valueOf(num);
+     int count []=new int[10];
+     for(char c:s.toCharArray()){
+        count[c-'0']++;
+     }
+     for(char c:s.toCharArray()){
+        int d= c-'0';
+        if(count[d]!=d)
+            return false;
+        
+     }
+     
+     return true;
 
-    private boolean isBalanced(int num) {
-        String s = String.valueOf(num);
-        int[] count = new int[10];
-
-        for (char c : s.toCharArray()) {
-            count[c - '0']++;
-        }
-
-        for (char c : s.toCharArray()) {
-            int d = c - '0';
-            if (count[d] != d) return false;
-        }
-
-        return true;
     }
 }

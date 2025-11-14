f199b8b3e2f597e56563077de352e200165c40c4
anchal singh <anchal1222005@gmail.com>
2025-11-15 00:15:34 +0530

    Time: 319 ms (6.06%) | Memory: 47.4 MB (6.07%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int left=0;
+            int right=0;
+            for(int l=0;l<i;l++){
+                left+=nums[l];
+            }
+            for(int r=i+1;r<nums.length;r++){
+                right+=nums[r];
+            }
+            if(left==right){
+                return i;
+            }
+        }
+        return -1;
+    }
+}

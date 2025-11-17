9b33da60453bae2b2473f44ade9866ca2f46caf9
anchal singh <anchal1222005@gmail.com>
2025-11-17 10:35:09 +0530

    Time: 1 ms (57.70%) | Memory: 43.7 MB (23.47%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<List<Integer>>res=new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        back(nums,0,new ArrayList<>());
+        return res;
+    }
+
+        public void back(int []nums,int index,List<Integer>temp){
+
+  if(index==nums.length){
+        res.add(new ArrayList<>(temp));
+        return;
+    }
+    temp.add(nums[index]);
+    back(nums,index+1,temp);
+    temp.remove(temp.size()-1);
+
+
+    //undo krega means exclude krrega jb [1,2,3] subset aa jyemge tb
+    back(nums,index+1,temp);
+        }
+
+        
+    }

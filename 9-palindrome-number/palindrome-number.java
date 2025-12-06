f965a49a5352361100f47e33e52470825883c93d
anchal singh <anchal1222005@gmail.com>
2025-12-07 00:31:28 +0530

    Time: 4 ms (100.00%) | Memory: 46 MB (25.13%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+           int s=0;
+        int target=x;
+        while(x>0){
+            int r=x%10;
+            s=(s*10)+r;
+            x=x/10;
+        }
+        return s== target;
+    }
+}
+    

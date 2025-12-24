5857f47a7faafabfed9aa2cdc3d4d19f43b53ec2
anchal singh <anchal1222005@gmail.com>
2025-12-24 21:53:46 +0530

    Time: 6 ms (99.51%) | Memory: 48 MB (32.26%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int minspeed=1;
+        int maxspeed=0;
+        for(int pile:piles){
+            maxspeed=Math.max(maxspeed,pile);
+
+        }
+        while(minspeed<maxspeed){
+            int mid=minspeed+(maxspeed-minspeed)/2;
+            if(caneat(piles,h,mid)){
+                maxspeed=mid;
+            }
+            else{
+                minspeed=mid+1;
+            }
+
+        }
+        return minspeed;
+    }
+    private boolean caneat(int [] piles,int h,int speed){
+        int hours=0;
+        for(int pile:piles){
+             hours += (pile + speed - 1) / speed;
+        }
+        return hours <= h;
+        }
+    
+}

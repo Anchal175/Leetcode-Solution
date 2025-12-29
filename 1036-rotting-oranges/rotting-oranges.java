72457b2f3567781e002e525d7bbefccbf77ca370
anchal singh <anchal1222005@gmail.com>
2025-12-29 17:32:05 +0530

    Time: 2 ms (89.04%) | Memory: 43.7 MB (91.90%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+
+        // Step 1: count fresh & add rotten to queue
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        int minutes = 0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        // Step 2: BFS
+        while (!q.isEmpty() && fresh > 0) {
+            int size = q.size();
+
+            for (int s = 0; s < size; s++) {
+                int[] curr = q.poll();
+                int r = curr[0], c = curr[1];
+
+                for (int[] d : dirs) {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
+
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n
+                            && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        q.offer(new int[]{nr, nc});
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        return fresh == 0 ? minutes : -1;
+    }
+}

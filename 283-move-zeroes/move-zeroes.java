21f64158d6bee1429f1f0afcc340c263ca390d6c
anchal singh <anchal1222005@gmail.com>
2026-05-19 09:22:22 +0530

    Time: 255 ms (5.23%) | Memory: 47.9 MB (19.23%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,53 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//   for(int i=0;i<nums.length-1;i++){
+//     for(int j=0;j<nums.length-1-i;j++){
+//         if(nums[j]==0&& nums[j+1]!=0){
+//             int temp=nums[j];
+//             nums[j]=nums[j+1];
+//             nums[j+1]=temp;
+//         }
+//     }
+//   }
+       
+//     }
+// }
+ class Solution {
+     public void moveZeroes(int[] nums) {
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             for (int j = 0; j < nums.length - 1 - i; j++) {
+//                 if (nums[j] == 0 && nums[j + 1] != 0) { // j use kiya
+//                     int temp = nums[j];
+//                     nums[j] = nums[j + 1];
+//                     nums[j + 1] = temp;
+//                 }
+//             }
+//         }
+//     }
+// }
+for(int j=0;j<nums.length-1;j++){
+for(int i=0;i<nums.length-j-1;i++){
+    if(nums[i]==0 && nums[i+1]!=0){
+        int temp=nums[i];
+        nums[i]=nums[i+1];
+        nums[i+1]=temp;
+    }
+}
+     }
+     }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

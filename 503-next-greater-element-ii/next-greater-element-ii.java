5144c4dff9fbb547d9f28f74941227e15335858b
anchal singh <anchal1222005@gmail.com>
2026-07-09 09:40:12 +0530

    Time: 90 ms (7.34%) | Memory: 47.4 MB (96.00%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,21 +1,38 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
 
-        int n = nums.length;
-        int[] ans = new int[n];
+        // int n = nums.length;
+        // int[] ans = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            ans[i] = -1; // default
+        // for (int i = 0; i < n; i++) {
+        //     ans[i] = -1; // default
 
-            for (int j = 1; j < n; j++) {
-                int idx = (i + j) % n; // circular index
+        //     for (int j = 1; j < n; j++) {
+        //         int idx = (i + j) % n; // circular index
 
-                if (nums[idx] > nums[i]) {
-                    ans[i] = nums[idx];
-                    break;
-                }
-            }
+        //         if (nums[idx] > nums[i]) {
+        //             ans[i] = nums[idx];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+int ans []=new int[nums.length];
+for(int i=0;i<nums.length;i++){
+    ans[i]=-1;
+    for(int j=1;j<nums.length;j++){
+        int k=(i+j)% nums.length;
+        if(nums[k]>nums[i]){
+            ans[i]=nums[k];
+            break;
         }
-        return ans;
+       
+    }
+}
+
+
+return ans;
+
     }
 }

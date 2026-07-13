c8b36a5f326b56e841307b8d1bfa3e363e6045ca
anchal singh <anchal1222005@gmail.com>
2026-07-14 00:42:57 +0530

    Time: 5 ms (14.04%) | Memory: 43.9 MB (5.02%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -22,25 +22,26 @@ class Solution{
             else return false;
         }
         return st.isEmpty();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
     }
 }
+// Stack<Character>st=new Stack<>();
+// for(int i=0;i<s.length();i++){
+//     if(s.charAt(i)=='('){
+//         st.push(')');
+//     }
+//     else if(s.charAt(i)=='{'){
+//         st.push('}');
+//     }
+//     else if(s.charAt(i)=='['){
+//         st.push(']');
+//     }
+//     else if(!st.isEmpty()&&(s.charAt(i)=='('|| s.charAt(i)=='{'||s.charAt(i)=='[')){
+//         if(st.peek()==s.charAt(i)){
+//             st.pop();
+//         }
+//         return false;
+//     }
+//     return false;
+// }
+// return st.isEmpty();
+// }}

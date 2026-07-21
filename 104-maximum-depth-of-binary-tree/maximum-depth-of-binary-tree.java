2c2b0988582de5b5efaa6a70cc500db7867a30ca
anchal singh <anchal1222005@gmail.com>
2026-07-21 09:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (15.69%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -18,7 +18,8 @@ class Solution {
         if (root == null) {
             return 0;
         }
-        
+        if(root.left==null && root.right==null)
+        return 1;
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
         

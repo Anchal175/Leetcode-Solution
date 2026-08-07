2607c000f63020ecfeb1193a1bf4233446e2a500
anchal singh <anchal1222005@gmail.com>
2026-08-07 09:06:55 +0530

    Time: 3 ms (40.74%) | Memory: 44.1 MB (22.61%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character>st=new Stack<>();
+        for(int  i=0;i<s.length();i++){
+            
+            if(Character.isDigit(s.charAt(i))){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+            }
+            String ans=sb.reverse().toString();
+            return ans;
+        
+    }
+}

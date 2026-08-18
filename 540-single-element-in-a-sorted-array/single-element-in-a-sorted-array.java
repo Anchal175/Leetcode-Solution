f6baa5ef30f8acaad97fb6e8d5e54bc7046d51ee
anchal singh <anchal1222005@gmail.com>
2026-08-18 23:52:00 +0530

    Time: 1 ms (25.06%) | Memory: 53.2 MB (9.00%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,18 +1,29 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        HashSet<Integer>map=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.contains(nums[i])){
-                map.remove(nums[i]);
-            }
-            else{
-                map.add(nums[i]);
-            }
-        }
-            for(int x:map){
-                return x;
-            }
+        // HashSet<Integer>map=new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.contains(nums[i])){
+        //         map.remove(nums[i]);
+        //     }
+        //     else{
+        //         map.add(nums[i]);
+        //     }
+        // }
+        //     for(int x:map){
+        //         return x;
+        //     }
         
-        return -1;
+        // return -1;
+
+
+
+
+
+        //by XOR SE
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor^=nums[i];
+        }
+        return xor;
     }
 }
